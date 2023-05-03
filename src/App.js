1f1a6ff99665bacd7d3f94efacc6eda4a4ae3b3e
SJasonHumphrey <sjasonhumphrey@gmail.com>
2023-05-03 16:39:06 -0500

    format changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
@@ -9,18 +9,18 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const locales = { "en-us": require ("date-fns/locale/en-US")};
+const locales = { "en-us": require("date-fns/locale/en-US") };
 
 const localizer = dateFnsLocalizer({
-   format,
-   parse,
-   startOfWeek,
-   getDay,
-   locales
-})
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 function App() {
   const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
@@ -28,26 +28,20 @@ function App() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => {
-    for (let i=0; i < allEvents.length; i++){
-
-      const d1 = new Date (allEvents[i].start);
+    for (let i = 0; i < allEvents.length; i++) {
+      const d1 = new Date(allEvents[i].start);
       const d2 = new Date(newEvent.start);
       const d3 = new Date(allEvents[i].end);
       const d4 = new Date(newEvent.end);
 
-       if (
-        ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-          (d4 <= d3) )
-        )
-      {   
-          alert("CLASH"); 
-          break;
-       }
-
-  }
+      if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
+        alert("Already Booked!");
+        break;
+      }
+    }
     setShow(false);
-    setAllEvents([...allEvents,newEvent])
-  }
+    setAllEvents([...allEvents, newEvent]);
+  };
   const handleShow = () => setShow(true);
 
   return (
@@ -62,9 +56,26 @@ function App() {
           <Modal.Title>Enter New Event</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+          <input
+            type="text"
+            placeholder="Add Title"
+            style={{ width: "20%", marginRight: "10px" }}
+            value={newEvent.title}
+            onChange={(e) =>
+              setNewEvent({ ...newEvent, title: e.target.value })
+            }
+          />
+          <DatePicker
+            placeholderText="Start Date"
+            style={{ marginRight: "10px" }}
+            selected={newEvent.start}
+            onChange={(start) => setNewEvent({ ...newEvent, start })}
+          />
+          <DatePicker
+            placeholderText="End Date"
+            selected={newEvent.end}
+            onChange={(end) => setNewEvent({ ...newEvent, end })}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -75,7 +86,13 @@ function App() {
           </Button>
         </Modal.Footer>
       </Modal>
-      <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 700, margin: "50px" }} />
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 700, margin: "50px" }}
+      />
     </div>
   );
 }
